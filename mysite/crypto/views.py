d2da81a099c45d5908e204645cdbff963afb1493
edgars61 <edgarsantana.62a@gmail.com>
2021-01-21 15:43:55 -0600

    connected to API and created cards with article previews

---
+++ mysite/crypto/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    import requests
+    import json
+    api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api = json.loads(api_request.content)
+    return render(request, 'home.html',{'api':api})
