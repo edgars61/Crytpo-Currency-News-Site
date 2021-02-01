34f2d6814c0439281e5fc772fb631831ee7cf5cf
edgars61 <edgarsantana.62a@gmail.com>
2021-02-01 11:12:20 -0600

    added function to view live price

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -5,9 +5,14 @@ from html.parser import HTMLParser
 def home(request):
     import requests
     import json
+
+    #Grab Crypto Price Data
+    price_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD")
+    price = json.loads(price_request.content)
+
+
+
+    #Grab Crypto News
     api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
     api = json.loads(api_request.content)
-    string =api
-    
-
-    return render(request, 'home.html',{'api':api})
+    return render(request, 'home.html',{'api':api,'price':price})
